a9c2b2183b40b91a0fed56fccb27a9aad5ec43b3
Ayman Ishimwe <ishimweay@mtn.co.rw>
2020-03-29 00:30:24 +0200

    feat(UI): Developed home page
    - Design and develop home page
    - Style the homepage elements
    - animate home page elements with jquery

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,12 @@
 |
 */
 
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('public.home');
 });
+
 Route::get('test','RoleController@index');
