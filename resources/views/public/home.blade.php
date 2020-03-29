7fedf2db1cab337c8786d88e2eecb79b645b1cd0
Ayman Ishimwe <ishimweay@mtn.co.rw>
2020-03-29 21:32:09 +0200

    feat(UI): UI enhancement
    - pull search box on the right
    - pull search button on the left
    - center brand name in relation to the logo
    - reduce width of the input search as well as the search button
    - make menu images part of clickable option

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -1,97 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('public.layouts.master')
 
-<head>
-    <!-- meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- main custom css -->
-    <link rel="stylesheet" href="./public_assets/css/main.css" />
-
-    <!-- Bootstrap css-->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-    <!-- Font Awesome -->
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <title>Classic Food Delivery</title>
-</head>
-
-<body>
-
-<nav class="navbar navbar-light navbar-expand-md bg-faded justify-content-center">
-    <a href="/" class="navbar-brand d-flex w-50 mr-auto">
-        <img src="./public_assets/images/cp logo.png" width="50" height="50" class="d-inline-block align-top" alt="Top-logo"> Classic Hotel
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar3">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="navbar-collapse collapse w-100" id="collapsingNavbar3">
-        <ul class="navbar-nav w-100 justify-content-center">
-            <li class="nav-item active">
-                <img src="./public_assets/images/menu-img.jpeg" class="nav-img" alt="menu-item" />
-                <a class="nav-link" href="#">All <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <img src="./public_assets/images/menu-img.jpeg" class="nav-img" alt="menu-item" />
-                <a class="nav-link" href="#">Main Menu</a>
-            </li>
-            <li class="nav-item">
-                <img src="./public_assets/images/menu-img.jpeg" class="nav-img" alt="menu-item" />
-                <a class="nav-link" href="#">Special Order</a>
-            </li>
-            <li class="nav-item">
-                <img src="./public_assets/images/menu-img.jpeg" class="nav-img" alt="menu-item" />
-                <a class="nav-link" href="#">Drinks</a>
-            </li>
-            <li class="nav-item">
-                <img src="./public_assets/images/menu-img.jpeg" class="nav-img" alt="menu-item" />
-                <a class="nav-link" href="#">Foreign Menu</a>
-            </li>
-        </ul>
-        <ul class="nav navbar-nav ml-auto w-100 justify-content-end">
-            <li class="nav-item dropdown">
-                <a href="#" class="btn btn-success my-2 my-sm-0 btn-cart-top" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    (3)<i class="fa fa-shopping-cart top-nav"></i>
-                </a>
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                    <div class="dropdown-cart-items">
-                        <div>2x Classic Special = <span class="red-text">Rwf 6500</span></div>
-                        <div class="dropdown-divider"></div>
-                        <div>2x Classic Special = <span class="red-text">Rwf 6500</span></div>
-                        <div class="dropdown-divider"></div>
-                        <div>2x Classic Special = <span class="red-text">Rwf 6500</span></div>
-                        <div class="dropdown-divider"></div>
-                        <div>
-                                <span class="float-left">
-                                    <a href="#" class="btn btn-sm btn-primary">
-                                        Go to cart
-                                    </a>
-                                </span>
-                            <span class="float-right">
-                                    <a href="#" class="btn btn-sm btn-success">
-                                        Checkout
-                                    </a>
-                                </span>
-                        </div>
-            </li>
-
-        </ul>
-    </div>
-</nav>
-
-
+@section('content')
 <div class="jumbotron">
     <div class="text-center">
         <form class="form-inline mx-auto d-block">
             <div class="form-row">
                 <div class="col-8">
-                    <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+                    <input class="form-control float-right" type="search" placeholder="Search" aria-label="Search">
 
                 </div>
 
                 <div class="col-4">
-                    <button class="btn btn-primary btn-main-search" type="submit">Search</button>
+                    <button class="btn btn-primary btn-main-search float-left" type="submit">Search</button>
                 </div>
             </div>
         </form>
@@ -291,45 +211,4 @@
         </div>
     </div>
 </div>
-<!-- Footer -->
-<footer class="page-footer font-small unique-color-dark pt-4">
-
-    <!-- Footer Elements -->
-    <div class="container">
-
-        <!-- Call to action -->
-        <ul class="list-unstyled list-inline text-center py-2">
-            <li class="list-inline-item">
-                <h5 class="mb-1">Contact us!</h5>
-            </li>
-        </ul>
-        <!-- Call to action -->
-
-    </div>
-    <!-- Footer Elements -->
-
-    <!-- Copyright -->
-    <div class="footer-copyright text-center py-3">© 2020 Copyright
-        <a href="https://classichotelkigali.com/"> classic hotel</a>
-    </div>
-    <!-- Copyright -->
-
-</footer>
-<!-- Footer -->
-
-
-
-<!--Jquery scripts -->
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-
-<!-- Bootstrap scripts -->
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
-<!-- Custom scripts -->
-<script src="./public_assets/js/scripts.js"></script>
-
-
-</body>
-
-</html>
+@endsection
