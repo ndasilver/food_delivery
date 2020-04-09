56c70fc008ea338f3198ebd9bbbd43d22894364b
Ayman Ishimwe <ishimweay@mtn.co.rw>
2020-04-09 23:58:37 +0200

    feat(UI): all Categories
    - Developed all categories page, with a sidemenu that displays all cacategories
    - Once a category is selected, menus that are associated to that specific category
    - Formatted all prices that are displayed on UI
    -

--- resources/views/public/categories.blade.php
+++ resources/views/public/categories.blade.php
@@ -3,14 +3,63 @@
 @section('content')
     <div class="jumbotron cart-page">
         <div class="text-center">
-            <h2 class="page-title">Cart</h2>
+            <h2 class="page-title">All Categories</h2>
         </div>
     </div>
-    <div class="container">
+    <div class="container-fluid">
 
         <div class="row">
-            <div class="col-md-4"></div>
-            <div class="col-md-8"></div>
+            <div class="col-md-3 sidebar">
+                @include('public.includes.sidemenu')
+            </div>
+            <div class="col-md-9 main-contents">
+                <div class="row">
+                    <?php $col_counter = 0 ?>
+                    @foreach($products as $product)
+
+                        <div class="col-md">
+                            <div class="row">
+                                <div class="col-4">
+                                    <img src="/public_assets/images/{{ $product->image }}" alt="menu-image" class="img-thumbnail menu-img">
+                                </div>
+                                <div class="col-8">
+                                    <div class="menu-title">
+                                        <h4>{{ $product->name }}</h4>
+                                    </div>
+                                    <div class="menu-desc">
+                                        <p>
+                                            {{ $product->description }}
+                                        </p>
+                                    </div>
+
+                                    <div class="">
+                            <span class="price float-left">
+                                <h5>{{ number_format($product->price) }} Rwf</h5>
+                            </span>
+
+                                        <span class="add-to-cart float-right">
+                                <button type="button" class="btn btn-primary btn-sm btn-add-to-cart" data-toggle="modal" data-target="#addToCartModal">
+                                    <i class="fa fa-plus-circle"></i> Add to cart
+                                </button>
+                            </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <?php
+                        $col_counter = $col_counter + 1;
+                        if($col_counter == 2){
+                            echo "<div class='w-100'></div>";
+
+                            $col_counter = 0;
+                        }
+
+                        ?>
+
+                    @endforeach
+                </div>
+            </div>
         </div>
     </div>
 
--- resources/views/public/category.blade.php
+++ resources/views/public/category.blade.php
@@ -6,62 +6,62 @@
             <h2 class="page-title">{{ $category->name }}</h2>
         </div>
     </div>
-    <div class="container">
+    <div class="container-fluid">
+
         <div class="row">
-            <div class="col-lg text-center">
-                <span class="text-center">
-                    <h3 class="display-4"> All {{ $category->name }} </h3>
-                </span>
+            <div class="col-md-3 sidebar">
+                @include('public.includes.sidemenu')
             </div>
+            <div class="col-md-9 main-contents">
+                <div class="row">
+                    <?php $col_counter = 0 ?>
+                    @foreach($products as $product)
 
-        </div>
+                        <div class="col-md">
+                            <div class="row">
+                                <div class="col-4">
+                                    <img src="/public_assets/images/{{ $product->image }}" alt="menu-image" class="img-thumbnail menu-img">
+                                </div>
+                                <div class="col-8">
+                                    <div class="menu-title">
+                                        <h4>{{ $product->name }}</h4>
+                                    </div>
+                                    <div class="menu-desc">
+                                        <p>
+                                            {{ $product->description }}
+                                        </p>
+                                    </div>
 
-        <div class="row menu-items">
-            <?php $col_counter = 0 ?>
-            @foreach($products as $product)
-
-                <div class="col-md">
-                    <div class="row">
-                        <div class="col-4">
-                            <img src="/public_assets/images/{{ $product->image }}" alt="menu-image" class="img-thumbnail menu-img">
-                        </div>
-                        <div class="col-8">
-                            <div class="menu-title">
-                                <h4>{{ $product->name }}</h4>
-                            </div>
-                            <div class="menu-desc">
-                                <p>
-                                    {{ $product->description }}
-                                </p>
-                            </div>
-
-                            <div class="">
+                                    <div class="">
                             <span class="price float-left">
-                                <h5>{{ $product->price }}</h5>
+                                <h5>{{ number_format($product->price) }} Rwf</h5>
                             </span>
 
-                                <span class="add-to-cart float-right">
+                                        <span class="add-to-cart float-right">
                                 <button type="button" class="btn btn-primary btn-sm btn-add-to-cart" data-toggle="modal" data-target="#addToCartModal">
                                     <i class="fa fa-plus-circle"></i> Add to cart
                                 </button>
                             </span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
+
+                        <?php
+                        $col_counter = $col_counter + 1;
+                        if($col_counter == 2){
+                            echo "<div class='w-100'></div>";
+
+                            $col_counter = 0;
+                        }
+
+                        ?>
+
+                    @endforeach
                 </div>
-
-                <?php
-                $col_counter = $col_counter + 1;
-                if($col_counter == 2){
-                    echo "<div class='w-100'></div>";
-
-                    $col_counter = 0;
-                }
-
-                ?>
-
-            @endforeach
-
+            </div>
         </div>
     </div>
+
+
 @endsection
